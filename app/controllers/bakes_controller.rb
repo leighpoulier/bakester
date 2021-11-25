03ab488eaba7121d66a165ab5411ad820d58f573
leighpoulier <86460742+leighpoulier@users.noreply.github.com>
2021-11-25 18:52:34 +1100

    fixed search form responsive reordering

--- app/controllers/bakes_controller.rb
+++ app/controllers/bakes_controller.rb
@@ -113,10 +113,10 @@ class BakesController < ApplicationController
   def search_params
     puts "SEARCH PARAMS"
     # There are two separate category selects on the view, because of layout issues.  They have two different parameters.
-    if params[:category_narrow] && !params[:category_narrow].empty? && (!params.include?(:category) || params[:category].empty? )
-      params[:category] = params[:category_narrow]
-      params.delete(:category_narrow)
-    end
+    # if params[:category_wide] && !params[:category_wide].empty? && (!params.include?(:category) || params[:category].empty? )
+    #   params[:category] = params[:category_wide]
+    #   params.delete(:category_wide)
+    # end
     if user_signed_in? && (current_user.admin || action_name == 'my_bakes')
       return params.permit(:active, :hidden, :search_text, :search_name, :search_description, :category, :sort_by, :sort_dir, :price_min, :price_max, :unit_price_min, :unit_price_max, :lead_time_min, :lead_time_max  )
     else
